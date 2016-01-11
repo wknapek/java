e2f99008dda70bd038ae3c3987e45473816eec1f
Witold Knapek <wknapek@gmail.com>
2016-01-11 21:14:52 +0100

    zad 5 in progres

--- zad5/src/ToDoLists.java
+++ zad5/src/ToDoLists.java
@@ -1,3 +1,12 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 /*
 zad 5 ver1
  */
@@ -5,8 +14,115 @@ zad 5 ver1
  *
  * @author raven
  */
-public
-    class ToDoLists
+public class ToDoLists implements ToDoListsInterface
 {
+    private Map<String ,List<listElem>> myMap = new TreeMap<>();
+    private static int counter = 0;
+    static synchronized int getID()
+    {
+        return counter++;
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////    
+    class listElem
+    {
+        String      Item;
+        int         Iditem;
+        ItemState   State;
+
+        listElem()
+        {
+            this.Iditem = getID();
+            this.State = ItemState.UNCHECKED;
+        }
+        listElem(String item)
+        {
+            this.Item = item;
+            this.Iditem = getID();
+            this.State = ItemState.UNCHECKED;
+        }
+    }
+    @Override
+    public void createToDoList(String name) throws AlreadyExistsException
+    {
+        myMap.put(name, new ArrayList<listElem>());
+    }
+
+    @Override
+    public void addItemToList(String itemName, String listName) throws AlreadyExistsException, DoesNotExistException
+    {
+        if(!myMap.containsKey(listName))
+        {
+            throw new DoesNotExistException();
+        }
+        List<listElem> myList = myMap.get(listName);
+        if(myList!=null)
+        {
+            for(int i = 0; i <myList.size();i++)
+            {
+                if(myList.get(i).Item.equals(itemName))
+                {
+                    throw new AlreadyExistsException();
+                }
+            }
+        }
+        myList.add(new listElem(itemName));
+        myMap.replace(listName, myList);
+    }
+
+    @Override
+    public Set<String> getLists()
+    {
+        Set<String> mySet = new TreeSet<>();
+        myMap.keySet().stream().forEach((key) ->
+        {
+            mySet.add(key);
+        });
+        return mySet;
+    }
+
+    @Override
+    public List<String> getItems(String listName) throws DoesNotExistException
+    {
+        List<String> myList = new ArrayList<>();
+        List<listElem> myItems = new ArrayList<>();
+        myItems = myMap.get(listName);
+        for(int i = 0; i < myItems.size(); i++)
+        {
+            myList.add(myItems.get(i).Item);
+        }
+        return myList;
+    }
+
+    @Override
+    public Integer getUniqItemId(String itemName, String listName) throws DoesNotExistException
+    {
+        int Id = -1;
+        List<listElem> myList = new ArrayList<>();
+        myList = myMap.get(listName);
+        for(int i = 0; i < myList.size();i++)
+        {
+            if(myList.get(i).Item==itemName)
+            {
+                Id = myList.get(i).Iditem;
+            }
+        }
+        if(Id == -1)
+        {
+            throw new DoesNotExistException();
+        }
+        return Id;
+    }
+
+    @Override
+    public ItemState getItemState(Integer itemID) throws DoesNotExistException
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void checkItem(Integer itemID) throws DoesNotExistException
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- zad5/src/Zad5.java
+++ zad5/src/Zad5.java
@@ -1,3 +1,9 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,17 +14,25 @@
  *
  * @author raven
  */
-public
-    class Zad5
+public class Zad5
 {
 
     /**
      * @param args the command line arguments
      */
-    public static
-        void main(String[] args)
+    public static void main(String[] args) throws ToDoListsInterface.AlreadyExistsException, ToDoListsInterface.DoesNotExistException
     {
-        // TODO code application logic here
+        ToDoLists tmp = new ToDoLists();
+        tmp.createToDoList("dupa");
+        tmp.addItemToList("test1", "dupa");
+        tmp.addItemToList("test2", "dupa");
+        tmp.createToDoList("dupa2");
+        Set<String> myset = new TreeSet<>();
+        myset = tmp.getLists();
+        List<String> myList = new ArrayList<>();
+        myList = tmp.getItems("dupa");
+        int tmp1;
+        tmp1 = tmp.getUniqItemId("test1", "dupa");
     }
     
 }
