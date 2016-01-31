957ba7d9b65033018a91c68f4d8330c216687b3c
Witold Knapek <wknapek@gmail.com>
2016-01-31 22:20:54 +0100

    zad 6 final

--- zad6/src/ReversiBoardExt.java
+++ zad6/src/ReversiBoardExt.java
@@ -2,14 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author dom
+ * zag 6 ver1
  */
 class ReversiBoardExt implements ReversiBoardInterfaceExt
 {
